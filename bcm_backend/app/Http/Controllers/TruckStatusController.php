3a45e4eeea750f524079f7fb26a7d2115a5199ee
bzimonja <30536574+bzimonja@users.noreply.github.com>
2023-01-22 14:11:49 -0800

    Add update to truck status functionality.

--- bcm_backend/app/Http/Controllers/TruckStatusController.php
+++ bcm_backend/app/Http/Controllers/TruckStatusController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Utils;
 use App\Models\TruckStatus;
 use Illuminate\Http\Request;
 use Log;
@@ -97,14 +97,93 @@ class TruckStatusController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
+     * Update the status of a truck.
+     *  Since each truck_status is only associated with a single truck, 
+     *  truck_id is mandatory, if truck_status id is not provided instead
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request)
     {
-        //
+        $response = array();
+
+        //check that the request is PUT or POST, with a valid non-empty JSON file
+        Utils::verifySaveRequest ($request);
+
+        $parameters = $request->json()->all();
+        
+        //check that mandatory parameters are set 
+        //  and of the appropriate type
+        $verify_message = Utils::verifyParametersTruckStatus($parameters, "update");
+        if ( $verify_message != null)            
+        {
+            $response["code"] = 4;
+            $response["mesage"] = $verify_message;
+
+            return response(json_encode($response), 400)->header('Content-Type', 'application/json');
+        }
+
+        // find the record with corresponding truck status id or truck id
+        $truck_status = new TruckStatus;
+        if (isset ($parameters["truck_status_id"]))
+        {
+            $truck_status = $truck_status->where("id", $parameters["truck_status_id"]);
+        }
+        if (isset ($parameters["truck_id"]))
+        {
+            $truck_status = $truck_status->where("truck_id", $parameters["truck_id"]);
+        }
+        $truck_status_to_update = $truck_status->first();   
+
+        // if status can't be found, return an error
+        if (!$truck_status_to_update)
+        {
+            $response["code"] = 5;
+            $response["message"] = "Database update failed. Could not find the truck status.";
+            return response(json_encode($response), 400)->header('Content-Type', 'application/json');
+        }   
+
+        //extract the update 
+        foreach ($parameters as $param_name => $param_value)
+        {
+            // check for the fields we want to update and update the existing entry
+            if ($param_name == "note" )
+            {
+                $truck_status_to_update->note = $param_value;
+            }
+            if ($param_name == "status_id" )
+            {
+                $truck_status_to_update->status_id = $param_value;
+            }
+            if ($param_name == "driver_id" )
+            {
+                $truck_status_to_update->driver_id = $param_value;
+            }
+            
+        }
+        $success = false;
+        $db_error_code = "";
+        try {
+            $success = $truck_status_to_update->save();
+        }
+        catch (\Illuminate\Database\QueryException $except)
+        {
+            $db_error_code= $except->getMessage();
+        }
+
+        if (!$success)
+        {
+            $response["code"] = 5;
+            $response["message"] = "Database update failed. Database error code ".$db_error_code;
+        }
+        else
+        {
+            $response["code"] = 0;
+            $response["message"] = "Database update successful.";
+        }
+
+        return response(json_encode($response), 400)->header('Content-Type', 'application/json');
+
     }
 
     /**
--- bcm_backend/app/Utils.php
+++ bcm_backend/app/Utils.php
@@ -78,10 +78,102 @@ class Utils
 					$return_message = "Truck label must be a string.";
 					return ($return_message);
 				}
+				break;
 			}
 			default:
 			{
 				$return_message = "Unknown operation requested.";
+			}
+		}
+		return ($return_message);
+
+	}
+
+	//method to verify input parameters for /api/truck_status entry points
+	public static function verifyParametersTruckStatus ($parameters, $action)
+	{
+		//for the assignment purposes, there are is only one action
+		// - upate, to update truck's status, and possibly assign/unassing a driver
+
+		$return_message = null;
+
+		switch ($action)
+		{
+			case 'update':
+			{
+
+				// truck_status table has internal id, which we check for first. 
+				//   it is possible to use the truck_id foreign key instead, as 
+				//   there is a strick 1:1 relationship between the two.				 
+				if (!isset($parameters["truck_status_id"]) || $parameters["truck_status_id"] == null)
+				{
+					if (!isset($parameters["truck_id"]) || $parameters["truck_id"] == null)
+					{
+						$return_message = "Truck status id or truck id are required.";
+						return ($return_message);
+					}
+				}
+
+				// at least two parameters are required, such as truck id and status id,
+				//  or truck status id and note
+				if (sizeof($parameters) < 2)
+				{
+					$return_message = "Too few parameters provided, at least two are required.";
+					return ($return_message);
+				}
+
+				// no more than five parameters are supported: truck id, status id,
+				//  truck status id, driver id and note
+				if (sizeof($parameters) > 5)
+				{
+					$return_message = "Too many parameters provided";
+					return ($return_message);
+				}
+
+				if (isset($parameters["truck_status_id"]) && ! is_int($parameters["truck_status_id"]))
+				{
+					$return_message = "Truck status id must be an integer.";
+					return ($return_message);
+				}
+
+				if (isset($parameters["truck_id"]) && ! is_int($parameters["truck_id"]))
+				{
+					$return_message = "Truck id must be an integer.";
+					return ($return_message);
+				}
+
+				if (isset($parameters["status_id"]) && ! is_int($parameters["status_id"]))
+				{
+					$return_message = "Status id must be an integer.";
+					return ($return_message);
+				}
+
+				if (isset($parameters["driver_id"]) && ! is_int($parameters["driver_id"]))
+				{
+					$return_message = "Driver id must be an integer.";
+					return ($return_message);
+				}
+
+				if (isset($parameters["note"]) && ! is_string($parameters["note"]))
+				{
+					$return_message = "Note must be a string.";
+					return ($return_message);
+				}
+
+				// if we provide truck status id or truck id, one of 
+				//   status id, driver id or note must be set
+				if (! isset($parameters["status_id"]) && ! isset($parameters["notes"]) 
+					&& !isset($parameters["driver_id"]))
+				{
+					$return_message = "At least one of status_id, driver_id or notes must be set.";
+					return($return_message);
+				}
+				break;
+			}
+			default:
+			{				
+				$return_message = "Unknown operation requested.";
+				return($return_message);
 			}
 		}
 		return ($return_message);
